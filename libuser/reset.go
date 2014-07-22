9cc3e5a3d4313c28cb84c478c17618052bce82a3
Kris Brandow <kris.brandow@gmail.com>
2014-07-22 13:38:10 -0400

    Adding reset.go: Functionality for password resetting.

---
+++ libuser/reset.go
@@ -0,0 +1,5 @@
+// Handles the password reset functionality for users
+package libuser
+
+// Password reset urls should be structed like this:
+// {root}/user/reset/{UUID-base64}/{timestamp}/{hash or unique token}
